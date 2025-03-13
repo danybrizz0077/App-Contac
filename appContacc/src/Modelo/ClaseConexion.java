f1a41bdc782c3689987ec7ec56a41f41f6f01a53
Daniel <danieloswaldo1007@gmail.com>
2025-03-13 08:00:01 -0600

    commit

--- appContacc/src/Modelo/ClaseConexion.java
+++ appContacc/src/Modelo/ClaseConexion.java
@@ -5,9 +5,9 @@ import java.sql.*;
 public class ClaseConexion {
     
        //Variables para la cadena de conexión
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String URL = "jdbc:oracle:thin:@192.168.1.117:1521:xe";
     private static final String USUARIO = "SYSTEM";
-    private static final String CONTRASENA = "ITR2024";
+    private static final String CONTRASENA = "1234567";
     //Creación del metodo de conexión que retorna la conexión
     public static Connection getConexion() {
         try {
